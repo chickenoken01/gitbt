e0bb18a4bffdea0bcc8dbe4f56ec5cde758d8189
chickenoken01 <chicuong6622@gmail.com>
2018-10-15 15:30:42 +0700

    commit jav04 by CC

--- source.cpp
+++ source.cpp
@@ -1,3 +1,17 @@
 int USCLN(int a, int b)
 {
+	int a,b;
+ cout<<"Nhap so a= ";
+ cin>>a;
+ cout<<"Nhap so b= ";
+ cin>>b;
+ while(a!=b)
+ {
+  if(a>b)
+  {
+   a=a-b;
+  }
+  else b=b-a;
+ }
+ cout<<"UCLN="<<a<<"\n";
 }
